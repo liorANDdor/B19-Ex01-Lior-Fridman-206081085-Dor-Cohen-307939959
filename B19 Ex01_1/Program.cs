c33904fbaaa4e5be19ced903ed6a21d17ab7ef18
Lior Fridman <liorfr@mta.ac.il>
2019-04-01 00:56:41 +0300

    j

--- B19 Ex01_1/Program.cs
+++ B19 Ex01_1/Program.cs
@@ -16,7 +16,11 @@ namespace B19_Ex01_1
 		{
 			int numInBinaryInput = GetInputFromUserInBinary();
 			int userInputInDecimal = BinaryToDecimalConvertor(numInBinaryInput);
-			Console.WriteLine(userInputInDecimal);
+			bool ch = CheckIfTheNumberSeriesIsAscending(162345);
+			int[] arr = { 3, 2, 3, 5 };
+			float av = CalculateAverage(arr, 4);
+			Console.WriteLine(av);
+			Console.WriteLine(ch);
 
 		}
 		public static int GetInputFromUserInBinary()
@@ -39,6 +43,33 @@ namespace B19_Ex01_1
 				i_numInBinaryInput = i_numInBinaryInput / 10;
 			}
 			return numInDecimal;
+		}
+		public static bool CheckIfTheNumberSeriesIsAscending (int i_numberToCheck)
+		{
+			bool v_checkNumIsAscending = true;
+			int lastDigit, oneDigitBeforeLast;
+			while (i_numberToCheck > 9 && v_checkNumIsAscending)
+			{
+				lastDigit = i_numberToCheck % 10;
+				oneDigitBeforeLast = (i_numberToCheck / 10) % 10;
+				if(oneDigitBeforeLast >= lastDigit)
+				{
+					v_checkNumIsAscending = false;
+				}
+				i_numberToCheck = i_numberToCheck / 10;
+			}
+			return v_checkNumIsAscending;
+		}
+		public static float CalculateAverage(int [] i_arrInput,int i_numOfElements)
+		{
+			float averageOfArrElements = 0;
+			int sumOfArrElements = 0;
+			for(int i=0;i<i_numOfElements;i++)
+			{
+				sumOfArrElements += i_arrInput[i];
+			}
+			averageOfArrElements = (float)sumOfArrElements / i_numOfElements;
+			return averageOfArrElements;
 		}
 	}
 }
