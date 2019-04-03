f9dbce1d7d405edb192d183ab1ff5443a23cc455
Lior Fridman <liorfr@mta.ac.il>
2019-04-03 16:23:54 +0300

    Section 5

--- B19 Ex01_5/Program.cs
+++ B19 Ex01_5/Program.cs
@@ -10,8 +10,45 @@ namespace B19_Ex01_5
 	{
 		public static void  Main()
 		{
-			string input = Console.ReadLine();
-
+			RunApplication();
+		}
+		public static void  RunApplication()
+		{
+			string inputFromUser = GetInputFromUser();
+			char maxDigit = inputFromUser.Max();
+			char minDigit = inputFromUser.Min();
+			int numOfDigAreDivideInThree = CheckNumOfDigAreDivideInThree(inputFromUser);
+			Console.WriteLine("The min digit is: {0}", minDigit);
+			Console.WriteLine("The max digit is: {0}", maxDigit);
+		}
+		public static string GetInputFromUser()
+		{
+			Console.WriteLine("Please enter a number with 8 digits: ");
+			string numberFromUser = Console.ReadLine();
+			bool v_inputIsValid = CheckIfInputIsValid(numberFromUser);
+			while(!v_inputIsValid)
+			{
+				Console.WriteLine("your input is invalid, try again ");
+				numberFromUser = Console.ReadLine();
+				v_inputIsValid = CheckIfInputIsValid(numberFromUser);
+			}
+			return numberFromUser;
+		}
+		public static bool CheckIfInputIsValid(string i_numberFromUser)
+		{
+			bool v_inputIsValid = true;
+			if (i_numberFromUser.Length != 8)
+			{
+				v_inputIsValid = false;
+			}
+			for (int i = 0; i < 8 && v_inputIsValid; i++)
+			{
+				if (i_numberFromUser[i] > '9' || i_numberFromUser[i] < '0')
+				{
+					v_inputIsValid = false;
+				}
+			}
+			return v_inputIsValid;
 		}
 	}
 }
